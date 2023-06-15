7e6bd341b8957e9dcbf0f667d5e8d2493d51930e
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-15 06:18:39 +0300

    Update room root apis

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -8,9 +8,28 @@ const auth = require('../../middleware/auth');
 const roomController = require('../../controllers/rooms');
 
 // @route           POST api/rooms
-// @description     Create Game Room
-// @access          Public
+// @description     Create Room
+// @access          Private
 router.post('/', auth, roomController.createRoom);
+
+// @route           PUT api/rooms/:id
+// @description     Update Room
+// @access          Private
 router.put('/:id', auth, roomController.updateRoom);
+
+// @route           GET api/rooms/:id
+// @description     Get Room By Id
+// @access          Private
+router.get('/:id', auth, roomController.getRoom);
+
+// @route           DELETE api/rooms/:id
+// @description     Delete Room by Id
+// @access          Private
+router.delete('/:id', auth, roomController.deleteRoom);
+
+// @route           GET api/rooms
+// @description     Get ALL Rooms
+// @access          Private
+router.get('/', auth, roomController.getAllRooms);
 
 module.exports = router;
