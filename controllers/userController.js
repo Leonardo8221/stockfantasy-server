e23f6dc186d1f4afe3d0ce332a0a02bf86d9c98c
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-15 03:03:15 +0300

    Create User Controller

---
+++ controllers/userController.js
@@ -0,0 +1,105 @@
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const normalize = require('normalize-url');
+
+const User = require('../models/User');
+
+const registerUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+    }
+
+    const avatar = normalize(
+      gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      }),
+      { forceHttps: true }
+    );
+
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_KEY,
+      { expiresIn: '5 days' },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+const updatePassword = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { oldPassword, newPassword } = req.body;
+
+    try {
+      let user = await User.findById(req.user.id);
+
+      if (user) {
+        const isPasswordMatched = await user.comparePassword(oldPassword);
+
+        if (!isPasswordMatched) {
+          res
+            .status(400)
+            .json({ errors: [{ message: 'Old password is incorrect' }] });
+          // return next(new ErrorHandler("Old password is incorrect", 400));
+        } else {
+          user.password = newPassword;
+          await user.save();
+
+          const payload = {
+            user: {
+              id: user.id
+            }
+          };
+          res.status(200).json({
+            success: [
+              { msg: 'Password Updated successfully', payload: payload }
+            ]
+          });
+        }
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send({ errors: err.errors });
+    }
+  }
+
+exports.registerUser = registerUser;
+exports.updatePassword = updatePassword;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,137 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator');
-const normalize = require('normalize-url');
-
-const User = require('../../models/User');
 const auth = require('../../middleware/auth');
+
+const userController = require('../../controllers/userController');
 
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
-router.post(
-  '/',
-  check('name', 'Name is required').notEmpty(),
-  check('email', 'Please include a valid email').isEmail(),
-  check(
-    'password',
-    'Please enter a password with 6 or more characters'
-  ).isLength({ min: 6 }),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+router.post('/', userController.registerUser);
 
-    const { name, email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'User already exists' }] });
-      }
-
-      const avatar = normalize(
-        gravatar.url(email, {
-          s: '200',
-          r: 'pg',
-          d: 'mm'
-        }),
-        { forceHttps: true }
-      );
-
-      user = new User({
-        name,
-        email,
-        avatar,
-        password
-      });
-
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
-
-      jwt.sign(
-        payload,
-        process.env.JWT_KEY,
-        { expiresIn: '5 days' },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
-    }
-  }
-);
-
-// @route           PUT api/users/update-password/:id
+// @route           PUT api/users/update-password
 // @description     Update password
-// @access          Public
-router.put(
-  '/update-password',
-  auth,
-  check('oldPassword', 'Please enter a current password').notEmpty(),
-  check(
-    'newPassword',
-    'Please enter a new password with 6 or more characters'
-  ).isLength({ min: 6 }),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      console.log('error: ' + errors.array());
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { oldPassword, newPassword } = req.body;
-
-    try {
-      let user = await User.findById(req.user.id);
-
-      if (user) {
-        const isPasswordMatched = await user.comparePassword(oldPassword);
-
-        if (!isPasswordMatched) {
-          res
-            .status(400)
-            .json({ errors: [{ message: 'Old password is incorrect' }] });
-          // return next(new ErrorHandler("Old password is incorrect", 400));
-        } else {
-          user.password = newPassword;
-
-          console.log('User', user);
-          await user.save();
-
-          const payload = {
-            user: {
-              id: user.id
-            }
-          };
-          console.log('Password Updated');
-          res.status(200).json({
-            success: [
-              { msg: 'Password Updated successfully', payload: payload }
-            ]
-          });
-        }
-      }
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send({ errors: err.errors });
-    }
-  }
-);
+// @access          Private
+router.put('/update-password', auth, userController.updatePassword);
 
 module.exports = router;
