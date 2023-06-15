79b8bca49da6a41eb3ccc1aa230ef38a5e1606f0
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-15 06:14:57 +0300

    Add getRoom, getAllRooms, deleteRoom functions

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -1,6 +1,25 @@
 const Room = require('../models/Room');
 const User = require('../models/User');
 
+const getAllRooms = async (req, res, next) => {
+  const rooms = await Room.findAll();
+
+  res.status(200).json({
+    success: true,
+    rooms,
+    numOfRooms: rooms.length
+  });
+};
+
+const getRoom = async (req, res, next) => {
+  const room = await Room.findById(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    room
+  });
+};
+
 const createRoom = async (req, res, next) => {
   const { name, type, players } = req.body;
 
@@ -36,5 +55,16 @@ const updateRoom = async (req, res, next) => {
   }
 };
 
+const deleteRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findByIdAndDelete(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
 exports.createRoom = createRoom;
 exports.updateRoom = updateRoom;
+exports.deleteRoom = deleteRoom;
+exports.getRoom = getRoom;
+exports.getAllRooms = getAllRooms;
