39c9406b5d36933dcd7cb574314be237809a1b56
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-21 00:08:10 +0300

    Add Score route

---
+++ routes/api/scores.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const normalize = require('normalize-url');
+
+const auth = require('../../middleware/auth');
+
+const scoreController = require('../../controllers/scores');
+
+// @route           POST api/scores
+// @description     Create Score
+// @access          Private
+router.post('/', auth, scoreController.createScore);
+
+// @route           PUT api/scores/:id
+// @description     Update Score
+// @access          Private
+router.put('/:id', auth, scoreController.updateScore);
+
+
+// @route           GET api/scores/
+// @description     Get Score By Id
+// @access          Private
+router.get('/', auth, scoreController.getScores);
+
+// @route           GET api/scores
+// @description     Get ALL Rooms
+// @access          Private
+router.get('/all', auth, scoreController.getAllScores);
+
+module.exports = router;
